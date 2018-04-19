4d4da453df4a94efdef94aa58ad95ad3cf6edb7e
gpasxalis <p12gram@ionio.gr>
2018-04-19 18:23:01 +0300

    With Mapping

--- myapp/views/hello2.py
+++ myapp/views/hello2.py
@@ -16,7 +16,27 @@ def test():
 	test = request.form.get('test')
 	test1 = request.form.get('test1')
 	#test = request.form["test"]
-	res = es.search(index=test, body={"query": {"match": {"data": test1}}})
+	#res = es.search(index="lib-index", body={"query": {"match": {"data": test1}}})  #1st Search
+	
+	
+	# Search After Mapping
+	
+	#res = es.search(index="lib-index", body={"query":{
+	#	"nested": {
+	#		"path": "datagroup",
+	#		"query":{
+	#			"bool":{
+	#				"should":[
+	#					{ "match": {"datagroup.data":"SMITH"}}
+	#				]
+	#				}
+	#			}
+	#		}
+	#	}})
+	
+	
+	res = es.search(index="lib-index", size=1000, body={"query": {"match": {"taggroup": test}}})  #1st Search
+					
 	return render_template("test.html", test=test, test1=test1, res=res)
 	
 	
